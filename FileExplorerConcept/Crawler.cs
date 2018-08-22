d13b8a3174986944b3a36ce41fb1b67841d43194
Night Next <marincalin2551@hotmail.com>
2018-08-22 18:09:17 +0300

    Add project files.

---
+++ FileExplorerConcept/Crawler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorerConcept
+{
+    class Crawler
+    {
+        private string _CurrentPath;
+        public string CurrentPath {
+            get
+            {
+                return _CurrentPath;
+            }
+            set
+            {
+                _CurrentPath = value;
+                CurrentDirectory = new DirectoryInfo(value);
+            }
+        }
+        public DirectoryInfo CurrentDirectory { get; set; }
+
+        public Crawler(string path)
+        {
+            CurrentPath = path;
+        }
+
+        public void EnterDirectory(string dirname)
+        {
+            CurrentPath = Path.Combine(CurrentPath, dirname);
+            if (!CurrentDirectory.Exists)
+            {
+                ExitDirectory();
+                throw new DirectoryNotFoundException();
+            }
+        }
+
+        public void ExitDirectory()
+        {
+            CurrentPath = CurrentDirectory.Parent.FullName;
+        }
+
+        public void PrintDirectory()
+        {
+            Console.Clear();
+            Console.WriteLine(CurrentPath);
+            Program.ColorMessage($"Directory Listing for {CurrentDirectory.Name}:", ConsoleColor.Green);
+            foreach (var entry in CurrentDirectory.GetDirectories())
+                Console.WriteLine($"-- {entry.Name}");
+            foreach (var entry in CurrentDirectory.GetFiles())
+                Console.WriteLine($"- {entry.Name}");
+            Console.WriteLine();
+            Program.ColorMessage("| - Type 'dir directory_name' to enter it.", ConsoleColor.Yellow);
+            Program.ColorMessage("| - Type 'start file_name' to run a file.", ConsoleColor.Yellow);
+            Program.ColorMessage("| - Type 'disk DISK (i.e C, D)' to run a file.", ConsoleColor.Yellow);
+            Program.ColorMessage("| - Use 'back' to go to parent.", ConsoleColor.Yellow);
+            Program.ColorMessage("| - Use 'exit' to exit program.", ConsoleColor.Yellow);
+        }
+
+        public void StartProgram(string name)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = Path.Combine(CurrentPath, name);
+            process.Start();
+        }
+
+        public void Disk(char disk)
+        {
+            CurrentPath = $@"{disk}:\";
+        }
+    }
+}
---
+++ FileExplorerConcept/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorerConcept
+{
+    class Program
+    {
+        public static Crawler Crawler { get; set; }
+
+        public static void ColorMessage(string message, ConsoleColor color)
+        {
+            var original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = original;
+        }
+
+        static void Main(string[] args)
+        {
+            Crawler = new Crawler(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+            Crawler.PrintDirectory();
+
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd.Equals("exit"))
+                {
+                    break;
+                }
+                else if (cmd.Equals("refresh"))
+                {
+                    Crawler.PrintDirectory();
+                }
+                else if(cmd.Equals("back"))
+                {
+                    try
+                    {
+                        Crawler.ExitDirectory();
+                        Crawler.PrintDirectory();
+                    }
+                    catch(Exception)
+                    {
+                        ColorMessage("Folder does not exist", ConsoleColor.Red);
+                    }
+                }
+                else if(cmd.Length > 4 && cmd.Substring(0, 4).Equals("dir "))
+                {
+                    try
+                    {
+                        Crawler.EnterDirectory(cmd.Substring(4));
+                        Crawler.PrintDirectory();
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        ColorMessage("Folder does not exist.", ConsoleColor.Red);
+                    }
+                }
+                else if (cmd.Length > 6 && cmd.Substring(0, 6).Equals("start "))
+                {
+                    var file = cmd.Substring(6);
+                    ColorMessage($"Starting {file}...", ConsoleColor.Green);
+                    try
+                    {
+                        Crawler.StartProgram(file);
+                    }
+                    catch (Win32Exception)
+                    {
+                        ColorMessage("File does not exist", ConsoleColor.Red);
+                    }
+                }
+                else if (cmd.Length == 6 && cmd.Substring(0, 5).Equals("disk "))
+                {
+                    var disk = cmd.Substring(5).ToUpper();
+                    ColorMessage($@"Moving to disk {disk}:\...", ConsoleColor.Green);
+                    Crawler.Disk(Convert.ToChar(disk));
+                    Crawler.PrintDirectory();
+                    ColorMessage($@"Moved to disk {disk}:\.", ConsoleColor.Green);
+                }
+                else
+                {
+                    Crawler.PrintDirectory();
+                }
+            }
+        }
+    }
+}
